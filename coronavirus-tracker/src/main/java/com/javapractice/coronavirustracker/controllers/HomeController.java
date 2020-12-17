f834ae5f469c187bf5da5941f71bb7d6a8d456f6
GaborNagy <gabor.nagy.1985@gmail.com>
2020-12-17 11:15:32 +0100

    Coronavirus-tracker application (javabrains.io)
    Coronavirus-tracker webapplication (keywords: Java 11, Spring Boot, Thymeleaf, Bootstrap 4.5)

---
+++ coronavirus-tracker/src/main/java/com/javapractice/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package com.javapractice.coronavirustracker.controllers;
+
+import com.javapractice.coronavirustracker.models.LocationStats;
+import com.javapractice.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private CoronaVirusDataService coronaVirusDataService;
+
+    @Autowired
+    public void setCoronaVirusDataService(CoronaVirusDataService coronaVirusDataService) {
+        this.coronaVirusDataService = coronaVirusDataService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat ->stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat ->stat.getDiffFromPrevDay()).sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
